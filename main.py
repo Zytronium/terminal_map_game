83235d2f817cea0178ffb06e9b9901466f7254d3
Zytronium <dastelljes@outlook.com>
2026-03-27 17:43:06 -0500

    Add player movement controls and implement keypress handling

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+import os
 import random
+import sys
+
 from colors import set_background_color, reset_color, set_color
 
 tiles = [
@@ -49,9 +52,24 @@ player = {
 }
 
 def main():
-    map = generate_map(15, 15, player["pos"], 10)
+    MAP_WIDTH = 25
+    MAP_HEIGHT = 15
+    map = generate_map(MAP_HEIGHT, MAP_WIDTH, player["pos"], 10)
 
-    print_map(map, player["pos"])
+    while True:
+        clear_screen()
+        print_map(map, player["pos"])
+        key = get_key()
+        if key == "up":
+            player["pos"] = (max(player["pos"][0] - 1, 0), player["pos"][1])
+        elif key == "down":
+            player["pos"] = (min(player["pos"][0] + 1, MAP_HEIGHT - 1), player["pos"][1])
+        elif key == "left":
+            player["pos"] = (player["pos"][0], max(player["pos"][1] - 1, 0))
+        elif key == "right":
+            player["pos"] = (player["pos"][0], min(player["pos"][1] + 1, MAP_WIDTH - 1))
+        elif key == "esc":
+            break
 
 def generate_map(x: int, y: int, player_pos: tuple, num_coins):
     map = []
@@ -114,7 +132,7 @@ def generate_map(x: int, y: int, player_pos: tuple, num_coins):
     # NOTE: If we add more items in the future, we will need to ensure that coins can't overwrite other items. We are safe for now since coins and volcanoes cannot spawn on the same tile types.
     for i in range(num_coins):
         pos = (random.randint(0, x - 1), random.randint(0, y - 1))
-        while not (map[pos[0]][pos[1]]["item"] == " " or map[pos[0]][pos[1]]["type"] in items[0]["spawns_on"] or pos not in coins):
+        while not (map[pos[0]][pos[1]]["item"] == " " and map[pos[0]][pos[1]]["type"] in items[0]["spawns_on"] and pos not in coins):
             pos = (random.randint(0, x - 1), random.randint(0, y - 1))
 
         coins.append(pos)
@@ -151,6 +169,68 @@ def print_map(map, player_pos):
             reset_color()
         print()
         j = -1
+
+
+def get_key():
+    """Get a single keypress (cross-platform)"""
+    if os.name == 'nt':  # Windows
+        import msvcrt
+        key = msvcrt.getch()
+        if key in [b'\xe0', b'\x00']:  # Arrow key prefix
+            key = msvcrt.getch()
+            if key == b'H':  # Up arrow
+                return 'up'
+            elif key == b'P':  # Down arrow
+                return 'down'
+            elif key == b'M':  # Right arrow
+                return 'right'
+            elif key == b'K':  # Left arrow
+                return 'left'
+        elif key == b'\r':  # Enter
+            return 'enter'
+        elif key == b'\x1b':  # Escape
+            return 'esc'
+        else:
+            # Return the actual character
+            try:
+                return key.decode('utf-8').lower()
+            except:
+                return None
+    else:  # Unix/Linux/Mac
+        import termios
+        import tty
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+            if ch == '\x1b':  # Escape sequence
+                ch2 = sys.stdin.read(1)
+                if ch2 == '[':
+                    ch3 = sys.stdin.read(1)
+                    if ch3 == 'A':  # Up arrow
+                        return 'up'
+                    elif ch3 == 'B':  # Down arrow
+                        return 'down'
+                    if ch3 == 'C':  # Right arrow
+                        return 'right'
+                    elif ch3 == 'D':  # Left arrow
+                        return 'left'
+                return 'esc'
+            elif ch == '\n' or ch == '\r':  # Enter
+                return 'enter'
+            else:
+                # Return the actual character (lowercased)
+                return ch.lower()
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return None
+
+
+def clear_screen():
+    """Clear the terminal screen"""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 
 if __name__ == "__main__":
